86496fce63b07dd935aa2426dc46a7a741bbd747
Srikanta <srikantagowda07@gmail.com>
2021-11-01 21:53:46 +0530

    style update

--- src/Dashboard/SideBar/SideBar.js
+++ src/Dashboard/SideBar/SideBar.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
-import inbox from "./assets/inbox.svg";
+// import inbox from "./assets/inbox.svg";
+import InboxIcon from "@mui/icons-material/Inbox";
+import TodayIcon from "@mui/icons-material/Today";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import { blue, lightGreen, purple } from "@mui/material/colors";
 import "./Style.scss";
 
 export const SideBar = ({}) => {
   return (
     <div className="sidebar">
       <div>
-        <img scr={inbox} alt="inbox" />
-        Inbox
+        <InboxIcon sx={{ color: blue[300] }} />
+        <span>Inbox</span>
       </div>
-      <div>Today</div>
-      <div>Upcoming</div>
+      <div>
+        <TodayIcon sx={{ color: lightGreen[500] }} />
+        <span>Today</span>
+      </div>
+
+      <div>
+        <DateRangeIcon sx={{ color: purple[500] }} />
+        <span>Upcoming</span>
+      </div>
     </div>
   );
 };
