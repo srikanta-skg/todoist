c7e8bf98111401da75a1d728eb33a3dac16ede6a
Srikanta <srikantagowda07@gmail.com>
2021-11-06 03:46:10 +0530

    filterToday task Function Update

--- src/Dashboard/Task/function.js
+++ src/Dashboard/Task/function.js
@@ -1,8 +1,17 @@
 import { DAYS, MONTHS } from "./constants";
+import isEmpty from "lodash/isEmpty";
 
 export const Time = (value) => {
   const d = value || new Date();
   return (
     DAYS[d.getDay() - 1] + " " + d.getDate() + " " + MONTHS[d.getMonth()]
   );
+};
+
+export const FilterTodayTaskOnly = (todoList) => { // This Function Filters Todays Task from all the task Present.
+  if (isEmpty(todoList)) return;
+  let result = todoList.filter((item) => {
+    return item.createdTime == Time() || item.complitionTime == Time();
+  });
+  return result;
 };
